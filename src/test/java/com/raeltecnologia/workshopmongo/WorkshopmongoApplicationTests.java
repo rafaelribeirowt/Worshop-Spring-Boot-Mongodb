223de795beada946ff2ba9ca030e0988f30d2a0e
Rafael Ribeiro <rafael.ribeirowt@gmail.com>
2022-10-26 16:57:42 -0300

    Projeto criado

---
+++ src/test/java/com/raeltecnologia/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raeltecnologia.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
