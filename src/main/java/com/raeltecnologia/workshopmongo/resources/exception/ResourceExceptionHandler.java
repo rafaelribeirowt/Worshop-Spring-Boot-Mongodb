24d38404a03cbc441bd174988e1043e90809bb73
Rafael Ribeiro <rafael.ribeirowt@gmail.com>
2022-10-26 20:11:33 -0300

    Obtendo um usuário por id

---
+++ src/main/java/com/raeltecnologia/workshopmongo/resources/exception/ResourceExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.raeltecnologia.workshopmongo.resources.exception;
+
+import java.time.Instant;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.raeltecnologia.workshopmongo.services.exception.ObjectNotFoundException;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+	
+	@ExceptionHandler(ObjectNotFoundException.class)
+	public ResponseEntity<StandardError> resourceNotFound(ObjectNotFoundException e, HttpServletRequest request ){
+		String error = " Resource not found";
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+		
+	}
+
+}
--- src/main/java/com/raeltecnologia/workshopmongo/services/UserService.java
+++ src/main/java/com/raeltecnologia/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.raeltecnologia.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.raeltecnologia.workshopmongo.domain.User;
 import com.raeltecnologia.workshopmongo.repository.UserRepository;
+import com.raeltecnologia.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -17,4 +19,12 @@ public class UserService {
 		public List<User> findAll(){
 			return repo.findAll();
 		}
-}
+		
+		public User findById(String id) {
+			Optional<User> obj = repo.findById(id);
+			return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+				
+				
+			}
+		}
+
