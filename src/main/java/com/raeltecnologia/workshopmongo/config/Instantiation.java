00d05f91215c081ae0bbe17cc92d84703669dd12
Rafael Ribeiro <rafael.ribeirowt@gmail.com>
2022-10-27 11:20:37 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/raeltecnologia/workshopmongo/config/Instantiation.java
+++ src/main/java/com/raeltecnologia/workshopmongo/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.raeltecnologia.workshopmongo.domain.Post;
 import com.raeltecnologia.workshopmongo.domain.User;
 import com.raeltecnologia.workshopmongo.dto.AuthorDTO;
+import com.raeltecnologia.workshopmongo.dto.CommentDTO;
 import com.raeltecnologia.workshopmongo.repository.PostRepository;
 import com.raeltecnologia.workshopmongo.repository.UserRepository;
 
@@ -28,7 +29,9 @@ public class Instantiation implements CommandLineRunner {
 		
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+		
 		userRepository.deleteAll();
+		postRepository.deleteAll();
 		
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
@@ -40,6 +43,13 @@ public class Instantiation implements CommandLineRunner {
 		
 		Post post2 = new Post (null, sdf.parse("27/10/2022"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 		
+		
+		CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("27/10/2022"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite!", sdf.parse("27/10/2022"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("tenha um otimo dia", sdf.parse("27/10/2022"), new AuthorDTO(alex));
+		
+		post1.getComments().addAll(Arrays.asList(c1,c2));
+		post2.getComments().addAll(Arrays.asList(c3));
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
--- src/main/java/com/raeltecnologia/workshopmongo/domain/Post.java
+++ src/main/java/com/raeltecnologia/workshopmongo/domain/Post.java
@@ -1,31 +1,34 @@
 package com.raeltecnologia.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.raeltecnologia.workshopmongo.dto.AuthorDTO;
+import com.raeltecnologia.workshopmongo.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	private String id;
 	private Date date;
 	private String title;
 	private String body;
 	private AuthorDTO author;
-	
-	
-	public Post() {
-		
-	}
 
+	private List<CommentDTO> comments = new ArrayList<>();
+
+	public Post() {
+
+	}
 
 	public Post(String id, Date date, String title, String body, AuthorDTO author) {
 		super();
@@ -36,61 +39,58 @@ public class Post implements Serializable {
 		this.author = author;
 	}
 
-
 	public String getId() {
 		return id;
 	}
-
 
 	public void setId(String id) {
 		this.id = id;
 	}
 
-
 	public Date getDate() {
 		return date;
 	}
-
 
 	public void setDate(Date date) {
 		this.date = date;
 	}
 
-
 	public String getTitle() {
 		return title;
 	}
-
 
 	public void setTitle(String title) {
 		this.title = title;
 	}
 
-
 	public String getBody() {
 		return body;
 	}
 
-
 	public void setBody(String body) {
 		this.body = body;
 	}
-	
+
 	public AuthorDTO getAutor() {
 		return author;
 	}
-
 
 	public void setAutor(AuthorDTO author) {
 		this.author = author;
 	}
 
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
+	}
 
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
 	}
-
 
 	@Override
 	public boolean equals(Object obj) {
@@ -104,8 +104,4 @@ public class Post implements Serializable {
 		return Objects.equals(id, other.id);
 	}
 
-
-	
-	
-	
 }
