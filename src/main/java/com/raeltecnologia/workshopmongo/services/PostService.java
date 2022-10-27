5368a528804a3899cee9705a3784fbf248d5f707
Rafael Ribeiro <rafael.ribeirowt@gmail.com>
2022-10-27 12:06:06 -0300

    Consulta simples com query methods

--- src/main/java/com/raeltecnologia/workshopmongo/services/PostService.java
+++ src/main/java/com/raeltecnologia/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.raeltecnologia.workshopmongo.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,4 +23,10 @@ public class PostService {
 
 	
 }
+	
+	public List<Post> findByTitle(String text){
+		return repo.findByTitleContainingIgnoreCase(text);
+		
+	}
+	
 }
