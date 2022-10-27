a1b4722417a896a67b144105c0020cffdde2e9ea
Rafael Ribeiro <rafael.ribeirowt@gmail.com>
2022-10-26 22:17:52 -0300

    Atualização de usuário com PUT

--- src/main/java/com/raeltecnologia/workshopmongo/resources/UserResource.java
+++ src/main/java/com/raeltecnologia/workshopmongo/resources/UserResource.java
@@ -52,5 +52,14 @@ public class UserResource {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
 	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+	public ResponseEntity<Void> update(@RequestBody UserDTO objDto, @PathVariable String id) {
+		User obj = service.fromDTO(objDto);
+		obj.setId(id);
+		obj = service.update(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.noContent().build();
+	}
 
 }
